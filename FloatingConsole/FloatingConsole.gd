751a48832aec62ec0c11626d0b55ed9042ef94f6
Chris <chris@christianarndt.net>
2022-01-12 08:09:36 +0100

    Added minimize and expand feature; Refactored parameteres

--- FloatingConsole/FloatingConsole.gd
+++ FloatingConsole/FloatingConsole.gd
@@ -6,8 +6,10 @@ signal input_err
 
 
 export var HISTORY_LENGTH = 10
-export var RESIZE_ICON_OFFSET = Vector2(-5, 40)
-export var COMMAND_PATTERN = "\\D\\D\\D\\s.+"
+export var RESIZE_ICON_OFFSET = Vector2(-10, 41)
+export var COMMAND_PATTERN = "^CMD\\s.*"
+export var WINDOW_TITLE = "Floating Console"
+export var ECHO_COMMAND = true
 
 
 var _regex;
@@ -24,7 +26,7 @@ func _ready():
 	_regex.compile(COMMAND_PATTERN)
 
 
-func _print(txt: String):
+func write(txt: String):
 	$VBoxContainer/ScrollContainer/LabelPrint.text += txt + "\n"
 	yield(get_tree().create_timer(0.1), "timeout")
 	$VBoxContainer/ScrollContainer.set_v_scroll(1000)
@@ -37,22 +39,23 @@ func _parse_command(cmd: String):
 		_command_history.remove(0)
 		_command_history_idx -= 1
 		
-	# echo
-	_print("< echo " + cmd)
-	
 	# parse
 	var result = _regex.search(cmd)
 	if result:
+		# emit parsed result
 		emit_signal("input_ok", result.get_string())
 	else:
+		# echo full input
+		write("< INPUT ERROR: command pattern: " + COMMAND_PATTERN)
 		emit_signal("input_err", cmd)
 
 
 func _on_LineEdit_text_entered(new_text):
-	_print("> " + new_text)
+	if ECHO_COMMAND:
+		write("> " + new_text)
 	_parse_command(new_text)
 	
-	$VBoxContainer/HBoxContainer/LineEdit.text = ""
+	$VBoxContainer/HBoxInput/LineEdit.text = ""
 	
 
 func _input(event):
@@ -66,14 +69,14 @@ func _input(event):
 		load_from_history = true
 	elif event.is_action_pressed("ui_cancel"):
 		_command_history_idx = 0
-		$VBoxContainer/HBoxContainer/LineEdit.text = ""
+		$VBoxContainer/HBoxInput/LineEdit.text = ""
 		
 	if load_from_history:
 		if _command_history_idx < 0:
 			_command_history_idx = len(_command_history) - 1
 		elif _command_history_idx > len(_command_history) - 1:
 			_command_history_idx = 0
-		$VBoxContainer/HBoxContainer/LineEdit.text = _command_history[_command_history_idx]
+		$VBoxContainer/HBoxInput/LineEdit.text = _command_history[_command_history_idx]
 		
 	# move or resize console window
 	if _dragging:
@@ -101,3 +104,20 @@ func _on_ColorRect_gui_input(event):
 			_resizing = true
 		else:
 			_resizing = false
+
+
+func _on_ButtonMin_button_up():
+	if $VBoxContainer/HBoxHeader/Button.text == "> <":
+		# minimize console window
+		$VBoxContainer/HBoxHeader/Label.size_flags_horizontal = SIZE_FILL
+		$VBoxContainer/HBoxHeader/Button.text = "< >"
+		$VBoxContainer/ScrollContainer.hide()
+		$ColorRect.hide()
+		$VBoxContainer/HBoxInput.hide()
+	elif $VBoxContainer/HBoxHeader/Button.text == "< >":
+		# maximize console window
+		$VBoxContainer/HBoxHeader/Label.size_flags_horizontal = SIZE_EXPAND_FILL
+		$VBoxContainer/HBoxHeader/Button.text = "> <"
+		$VBoxContainer/ScrollContainer.show()
+		$ColorRect.show()
+		$VBoxContainer/HBoxInput.show()
